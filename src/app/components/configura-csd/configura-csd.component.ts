2f20ba2d8c141e9a6eabdd68a71a38cc7387cb45
ovaldezb <omar.valdez.becerril@gmail.com>
2025-08-12 17:18:59 -0600

    edita y borra sucursal

--- src/app/components/configura-csd/configura-csd.component.ts
+++ src/app/components/configura-csd/configura-csd.component.ts
@@ -6,8 +6,6 @@ import { CertificadosService } from '../../services/certificados.service';
 import { Certificado } from '../../models/certificado';
 import { SucursalService } from '../../services/sucursal.service';
 
-
-
 @Component({
   selector: 'app-configura-csd',
   standalone: true,
@@ -16,26 +14,31 @@ import { SucursalService } from '../../services/sucursal.service';
 })
 export class ConfiguraCsdComponent implements OnInit{
     public certificados:Certificado[] = [];
-    public mostrarFormulario = false;
+    public mostrarFormulario: boolean = false;
+    public isLoadingCerts: boolean = false; // Variable para controlar el estado de carga
+    public isSavingSucursal: boolean = false;
     public csdSeleccionado: Certificado | null = null;
-    public mostrarModal = false;
+    public mostrarModal: boolean = false;
     public nuevaSucursal: Sucursal = new Sucursal('','','','','','','');
     public csdActual: Certificado = new Certificado('', '', '', '', new Date(), new Date(), [], '' );
-    constructor(private certificadoService: CertificadosService, private sucursalService: SucursalService) {
-    // Inicializar el certificado en el constructor
-    
-   }
-  //public certificado:Certificado=new Certificado('Juan Valdez','VABO780711D41','00000258',new Date(),new Date(),['suc01','suc02'],'ovaldez');
-ngOnInit(): void {
-
-    this.certificadoService.getAllCertificados()
-    .subscribe({
-        next: (res) => {
-            this.certificados = res.body || [];            
-        },
-        error: (error) => {
-            console.error('Error al obtener certificados:', error);
-        }
+    public editandoSucursal: boolean = false;
+    public sucursalAEditar: Sucursal | null = null;
+    constructor(private certificadoService: CertificadosService, private sucursalService: SucursalService) {}
+  
+    ngOnInit(): void {
+        this.loadCerts();
+    }
+    loadCerts(): void {
+        this.isLoadingCerts = true; // Cambiar el estado de carga a verdadero
+        this.certificadoService.getAllCertificados()
+        .subscribe({
+            next: (res) => {
+                this.isLoadingCerts = false; // Cambiar el estado de carga a falso
+                this.certificados = res.body || [];            
+            },
+            error: (error) => {
+                console.error('Error al obtener certificados:', error);
+            }
     });
 
     /*if(this.certificadoService){
@@ -60,13 +63,13 @@ ngOnInit(): void {
         this.csdSeleccionado = null;
     }
 
-  seleccionarCSD(certificado: Certificado) {
-    if (this.csdSeleccionado?._id === certificado._id) {
-      this.csdSeleccionado = null;
-    } else {
-      this.csdSeleccionado = certificado;
+    seleccionarCSD(certificado: Certificado) {
+        if (this.csdSeleccionado?._id === certificado._id) {
+            this.csdSeleccionado = null;
+        } else {
+            this.csdSeleccionado = certificado;
+        }
     }
-  }
 
   guardarCSD() {
     // Implementar lógica de guardado
@@ -78,20 +81,26 @@ ngOnInit(): void {
     this.mostrarModal = true;
   }
 
+  editarSucursal(sucursal: Sucursal) {
+    this.editandoSucursal = true;
+    this.sucursalAEditar = sucursal;
+    this.nuevaSucursal = { ...sucursal }; // Copiar los datos de la sucursal para editar
+    this.mostrarModal = true;
+  }
+
   guardarSucursal() {
+    this.isSavingSucursal = true;
     if (this.csdActual && this.nuevaSucursal.codigo_sucursal) {
       this.nuevaSucursal.id_certificado = this.csdActual._id; // Asignar el ID del certificado actual a la nueva sucursal
-      console.log('certificado actual arriba:', this.csdActual);
       const certificado = this.csdActual; // Guardar el certificado actual antes de la llamada al servicio
       this.sucursalService.insertarSucursal(this.nuevaSucursal).subscribe({
         next: (res) => {
-            console.log('certificado actual:', certificado);
-            //this.nuevaSucursal.id = res.body.id; // Asignar el ID de la sucursal recién creada
             certificado.sucursales.push(new Sucursal(res.body.id,'', '', '', '', '', ''));
             this.certificadoService.updateCertificado(certificado)
             .subscribe({
                 next: (updateRes) => {
-                    console.log('Certificado actualizado con nueva sucursal:', updateRes);
+                    //console.log('Certificado actualizado con nueva sucursal:', updateRes);
+                    this.isSavingSucursal = false;
                     this.cerrarModal();
                 },
                 error: (error) => {
@@ -107,8 +116,14 @@ ngOnInit(): void {
     }
   }
 
+  borrarSucursal(sucursal: Sucursal): any {
+    // Implementar lógica de borrado
+  } 
+
   cerrarModal() {
     this.mostrarModal = false;
+    this.editandoSucursal = false;
+    this.sucursalAEditar = null;
     this.csdActual = {} as Certificado; // Limpiar el certificado actual};
     this.nuevaSucursal = new Sucursal('', '', '', '', '','', '');
   }
