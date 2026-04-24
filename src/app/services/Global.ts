df5d6ccaa1e45c540ccfcb14ea6b4d772cbde010
omar valdez <omar.valdez.becerril@gmail.com>
2026-04-24 12:36:39 -0600

    pump version

--- src/app/services/Global.ts
+++ src/app/services/Global.ts
@@ -1,7 +1,7 @@
 import { environment } from "../../environments/environment";
 
 export const Global = {
-  VERSION: '1.3.1',
+  VERSION: '1.3.2',
   ENV_NAME: environment.envName,
   urlBackEnd: environment.urlBackEnd,
   urlDatosFactura: environment.urlDatosFactura,
